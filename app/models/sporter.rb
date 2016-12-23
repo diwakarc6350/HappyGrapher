44df5d7dcd266b5d527d91d13a845be28ca7f613
Diwakar Chand <dchand@covermymeds.com>
2016-12-23 15:23:42 -0500

    added geo charts

--- app/models/sporter.rb
+++ app/models/sporter.rb
@@ -1,3 +1,16 @@
 class Sporter < ApplicationRecord
   belongs_to :country
+
+  def self.sporter_by_country
+    clean_maps ={}
+    hash_maps = find_by_sql("select c.name as country, count(c.id) as count from sporters s join countries c where s.country_id=c.id group by c.name order by count desc;").map(&:attributes)
+    hash_maps.each do |hash_map|
+      hash_map.delete('id')
+    end
+    hash_maps.each do |hash_map|
+      clean_maps[hash_map['country']]=hash_map['count']
+    end
+    return clean_maps
+  end
+
 end
